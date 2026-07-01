cf19a9c759db596a91f15bb03c6241906fac83a7
WareeshaAnjum <wareeshaanjum2004@gmail.com>
2026-07-01 21:13:19 +0530

    first sommit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-function search(){
+ async function search(){
     let city= document.querySelector('.outer input').value
     const key='d4c2f19e99bb34c2142941b784fd4cfc';
-    const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
-    let res= fetch(url)
+    const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    
+    let res=  await fetch(url)
+    console.log(res);
+    // let a= 30;
+    // let b= 40;
+    // console.log(a+b);
+    let data= await res.json();
+    console.log(data);
+    document.querySelector('.outer h3').innerHTML= `City: ${data.name}`;
+    document.querySelector('.outer h4').innerHTML= `Temp: ${data.main.temp}°C`
+    
 }// api 
+ 
